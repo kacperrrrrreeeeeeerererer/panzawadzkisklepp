3ced7c153aff59afbc04b6619f48d9aee7652b8e
kacperrrrrreeeeeeerererer <kacper.marchilewicz@teb.edu.pl>
2025-04-02 09:46:26 +0200

    Add files via upload

---
+++ ff/kupauto.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <link rel="stylesheet" href="styl.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Komis aut</title>
+</head>
+<body>
+            <!-- kontenery -->
+        <main id="banner">
+        <h1> <em><b>KupAuto!</b></em> Internetowy Komis Samochodowy </h1> </main>          
+        <main id="pierwszy-blok-glowny"> <img src="ToyotaYaris.jpg" alt="oferta dnia"> <h4>Oferta dnia: Toyota</h4> <p>Rocznik:1999, przebieg: 30000km, rodzaj paliwa: Paliwo </p> <h4>cena:4000 </h4></main>
+        <main id="drugi-blok-glowny">  </main>
+        <main id="trzeci-blok-glowny"> skrypt 3</main>
+        <main id="stopka"> <p>Strone wykonal:121</p> <p> <a href="http://firmy.pl/komis"> znajdz nas takze </a> </p></main>
+
+</body>
+</html
